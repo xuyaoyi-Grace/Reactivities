43e629a6cf858ba6a3d5fb89e21113a9640c1fa3
Grace Xu <xuyaoyi_grace@hotmail.com>
2021-07-14 14:54:33 +1000

    end of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
+import 'react-calendar/dist/Calendar.css';
+import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';

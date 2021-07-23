b71775b48ce6ac90af4011b6d8b5556292dc36ce
Grace Xu <xuyaoyi_grace@hotmail.com>
2021-07-23 20:53:23 +1000

    end of section 20

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -7,9 +7,10 @@ namespace Domain
     {
         public string DisplayName { get; set; }
         public string Bio { get; set; }
-
-         public ICollection<ActivityAttendee> Activities { get; set; }
-         public ICollection<Photo> Photos { get; set; }
+        public ICollection<ActivityAttendee> Activities { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+        public ICollection<UserFollowing> Followings { get; set; }
+        public ICollection<UserFollowing> Followers  { get; set; }
 
     }
 }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -14,6 +14,7 @@ namespace Persistence
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<UserFollowing> UserFollowings { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -21,6 +22,14 @@ namespace Persistence
             builder.Entity<ActivityAttendee>().HasOne(u => u.AppUser).WithMany(a => a.Activities).HasForeignKey(aa => aa.AppUserId);
             builder.Entity<ActivityAttendee>().HasOne(a => a.Activity).WithMany(u => u.Attendees).HasForeignKey(aa => aa.ActivityId);
             builder.Entity<Comment>().HasOne(a => a.Activity).WithMany(c => c.Comments).OnDelete(DeleteBehavior.Cascade);
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new {k.ObserverId, k.TargetId});
+
+                b.HasOne(o => o.Observer).WithMany(f => f.Followings).HasForeignKey(o => o.ObserverId).OnDelete(DeleteBehavior.Cascade);
+
+                b.HasOne(t => t.Target).WithMany(f => f.Followers).HasForeignKey(t => t.TargetId).OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }

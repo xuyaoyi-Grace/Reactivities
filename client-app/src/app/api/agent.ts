5a84253b6fc4384348651f4983cc51d836e6ac8a
Grace Xu <xuyaoyi_grace@hotmail.com>
2021-07-24 21:40:45 +1000

    end of section 21

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { profile } from 'console';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/activity';
+import { PaginatedResult } from '../models/pagination';
 import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
@@ -23,6 +24,11 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+    const pagination = response.headers['pagination']
+    if (pagination) {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>
+    }
     return response;
 }, (error:AxiosError) => {
     const {data, status, config} = error.response!;
@@ -65,7 +71,7 @@ const requests = {
 } 
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', {params}).then(responseBody),
     details: (id:string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity:ActivityFormValues) => requests.post<void>('activities', activity),
     update: (activity:ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
@@ -94,7 +100,8 @@ const Profiles = {
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
     editProfile: (profile: Partial<Profile>) => requests.put('/profiles', profile),
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
-    listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+    listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+    getUserActivities:(username:string, params: URLSearchParams) => axios.get(`/profiles/${username}/activities`, {params}).then(responseBody)
 }
 
 const agent = {
